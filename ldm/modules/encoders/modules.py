b0d2609414b390e59fe204911e3fe63dd7afb4c5
crumb <58605641+aicrumb@users.noreply.github.com>
2022-09-09 10:44:25 -0800

    Update modules.py

--- ldm/modules/encoders/modules.py
+++ ldm/modules/encoders/modules.py
@@ -160,7 +160,7 @@ class FrozenCLIPEmbedder(AbstractEncoder):
 #     def __init__(self, version="openai/clip-vit-large-patch14", device="cuda", max_length=77):
     def __init__(self, version="CompVis/stable-diffusion-v1-4", device="cuda", max_length=77): # testing to see if this works, bc i keep getting a json error
         super().__init__()
-        self.tokenizer = CLIPTokenizer.from_pretrained(version, subfolder="text_encoder", use_auth_token=True)
+        self.tokenizer = CLIPTokenizer.from_pretrained("openai/clip-vit-large-patch14")
         self.transformer = CLIPTextModel.from_pretrained(version, subfolder="text_encoder", use_auth_token=True)
         self.device = device
         self.max_length = max_length
